fad92d31742ff15cf6e5639a8e84a724741ad079
Constantin Dumitrescu <dum.constantin@gmail.com>
2019-11-14 18:57:54 +0200

    refactor(view): Move and improve attribute/props handling

--- src/lib/view/props/calculateExtraProps.ts
+++ src/lib/view/props/calculateExtraProps.ts
@@ -1,10 +1,38 @@
 import kebabCase from 'kebab-case';
 
 export function calculateExtraProps(props: any, el: any) {
-  let extraProps = Object.keys(props.receivedProps.props).reduce((acc, x) => {
-    acc[`data-props-${kebabCase(x)}`] = props.receivedProps.props[x];
+  const rProps = props.rProps;
+  const propsStructure = props.propsStructure;
+
+  const receivedProps = Object.keys(rProps).reduce(
+    (acc, x) => {
+      if (/^data\-/.test(x)) {
+        acc.data[x] = rProps[x];
+      } else if (/^aria\-/.test(x)) {
+        acc.aria[x] = rProps[x];
+      } else if (/^role/.test(x)) {
+        acc.role = rProps[x];
+      } else if (/^className/.test(x)) {
+        acc.className = rProps[x];
+      } else {
+        if (propsStructure.external.includes(x)) {
+          acc.props[x] = rProps[x];
+        }
+      }
+      return acc;
+    },
+    {
+      className: undefined,
+      data: {},
+      props: {},
+      aria: {},
+      role: undefined
+    } as any
+  );
+  let extraProps = Object.keys(receivedProps.props).reduce((acc, x) => {
+    acc[`data-props-${kebabCase(x)}`] = receivedProps.props[x];
     return acc;
-  }, Object.assign({}, props.receivedProps.data));
+  }, Object.assign({}, receivedProps.data));
 
   extraProps = Object.keys(props.state).reduce((acc, x) => {
     const name = `data-values-${kebabCase(x)}`;
@@ -12,18 +40,19 @@ export function calculateExtraProps(props: any, el: any) {
     return acc;
   }, extraProps);
 
-  if (props.receivedProps.className) {
-    extraProps.className = props.receivedProps.className;
+  if (receivedProps.className) {
+    extraProps.className = receivedProps.className;
     if (el.props.className) {
       // TODO: Dedupe class names
       extraProps.className += ' ' + el.props.className;
     }
   }
 
-  extraProps = Object.assign(extraProps, props.receivedProps.aria);
+  extraProps = Object.assign(extraProps, receivedProps.aria);
 
-  if (props.receivedProps.role) {
-    extraProps['role'] = props.receivedProps.role;
+  if (receivedProps.role) {
+    extraProps['role'] = receivedProps.role;
   }
+
   return extraProps;
 }
