092d7d6e15a82e74a9a0df17ec16567cd11aad3a
Radu Cocieru <radu@code11.com>
2019-11-20 14:39:17 +0200

    added View Macro

--- README.md
+++ README.md
@@ -15,3 +15,21 @@ export default {
   )
 };
 ```
+
+
+```javascript
+import view from '@code11/ui-engine/view.macro';
+import { IGet } from '@code11/ui-engine';
+import State from "./state";
+
+const Get = IGet<State>;
+
+export default view((
+    foo = Get['@prop1'],
+    bar = Get.bar
+ ) => (
+  <div>
+    {foo} {bar}
+  </div>
+));
+```
---
+++ src/specs/view.macro.spec.ts
@@ -0,0 +1,49 @@
+const pluginTester = require("babel-plugin-tester").default
+const prettier = require("prettier")
+const plugin = require("babel-plugin-macros")
+
+pluginTester({
+  plugin,
+  snapshot: false,
+  babelOptions: {
+    filename: __filename,
+  },
+  formatResult: (result: any) => {
+    return prettier.format(result);
+  },
+  tests: {
+    'does not change this': '\n\nexport const State = initialState;\n\n',
+    'does not change this2': 'module.exports.State = intiailState;',
+    'changes this code': {
+      code: `
+import React from 'react'
+import view from '../macros/view.macro'
+import ReactDom from 'react-dom'
+export default view((
+  isOpen = Get.notificationsPopup.visible,
+  openPopUp = Set.currentPopup,
+  t = Get.utils.i18n
+) => {
+  console.log('Icon render', isOpen, openPopUp)
+  return !isOpen && "HTML"
+})
+    `,
+      output: `
+import React from "react";
+import { view } from "@code11/ui-engine";
+import ReactDom from "react-dom";
+export default view({
+  args: {
+    isOpen: ["Get", "notificationsPopup", "visible"],
+    openPopUp: ["Set", "currentPopup"],
+    t: ["Get", "utils", "i18n"]
+  },
+  fn: ({ isOpen, openPopUp, t }) => {
+    console.log("Icon render", isOpen, openPopUp);
+    return !isOpen && "HTML";
+  }
+});
+    `
+    }
+  }
+})
