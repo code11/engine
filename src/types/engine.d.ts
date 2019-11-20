5aa04322dc653ef9676092b90d2f0c1a4e58cc00
Constantin Dumitrescu <dum.constantin@gmail.com>
2019-11-20 18:32:35 +0200

    fix(types): engine had experiments

--- src/types/engine.d.ts
+++ src/types/engine.d.ts
@@ -34,31 +34,3 @@ export interface View extends Module {
 export interface Producer extends Module {
   fn: Produce;
 }
-
-import { view } from '../lib/view';
-
-export default((
-  title,
-  // tempValue = Set.view['$id'].tempValue
-  submit,
-  theme,
-  something = Set.views['$id'].state.something,
-  somethingVal = Get.views['$id'].state.something,
-  some = Path.views['$id'].state['@r']
-) => <div>{title}
-<input onUpdate={tempValue(this.value)} />
-<button onSubmit={something(some.get({r:ev.target}))}
-</div>)
-
-interface ListItem {
-  title: string
-}
-
-import listItem from 'components/listItem'
-
-// ListItem for this specific app
-export default view((
-  title: Get.foo.bar,
-  submit: Set.foo.baz,
-) => <listItem titl={title} submit={submit} />
-
