3676e14a03c61f21f134cbafbb2bc9c1a60830a7
Constantin Dumitrescu <constantin.dumitrescu@code11.com>
2019-11-20 19:08:06 +0200

    test(view): import propsComponent to also run in the tests

--- src/lib/view/propsComponent.tsx
+++ src/lib/view/propsComponent.tsx
@@ -8,6 +8,6 @@ import { BaseProps, BaseState } from './types';
  * - Looks over errors that might occur in the state manager
  * and handles them accordingly
  */
-export default function propsComponent() {
+export function propsComponent() {
   return class PropsComponent extends React.Component<BaseProps, BaseState> {};
 }
