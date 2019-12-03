70763f7f3e6cf81442fe426b899ad9f1de47540c
Constantin Dumitrescu <dum.constantin@gmail.com>
2019-12-03 10:55:28 +0200

    style(macro): remove comments from index and processReferences

--- src/macro/index.macro.ts
+++ src/macro/index.macro.ts
@@ -2,30 +2,6 @@ import { createMacro } from 'babel-plugin-macros';
 import * as Babel from '@babel/core';
 import { processReference } from './processReference';
 
-// export as engine.macro
-
-// Func, struct and value are implicit
-
-// external: @
-// internal: \$
-// invokable: :
-
-// Walk through all the calls of the macro
-// Check if the call is valid
-// Get the data structure
-// Process the structure recursively
-// Each node can be either:
-// a) identifier
-// This would be equivalent to GET['@prop']
-// b) assignmentPattern
-// - process assignment pattern:
-// - - a) a PathType
-// - - b) a composition
-// - - c) new structure
-//
-// Create the final structure
-// Replace the macro arguments with the final structure
-// Insert the producer/view import
 type References = Babel.NodePath[];
 
 interface MacroParams {
