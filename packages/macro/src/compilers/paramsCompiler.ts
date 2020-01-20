8b2ace88c7ac0ac2418e49ecaf48238e22c071aa
Constantin Dumitrescu <constantin.dumitrescu@code11.com>
2020-01-20 21:50:18 +0200

    feat: change fn invokation to ordered params and add meta.order on struct

--- packages/macro/src/compilers/paramsCompiler.ts
+++ packages/macro/src/compilers/paramsCompiler.ts
@@ -3,8 +3,14 @@ import { Identifier, identifier } from "@babel/types";
 
 export const paramsCompiler = (struct: StructOperation): Identifier[] => {
   const value = struct.value;
-  const result = Object.keys(value)
-    .sort((a, b) => value[a].meta.order - value[b].meta.order)
-    .map(x => identifier(x));
+  let result;
+  if (struct.meta && struct.meta.order) {
+    result = struct.meta.order.map((x: string) => identifier(x));
+  } else {
+    throw new Error(
+      "Missing arguments order from struct" + JSON.stringify(struct, null, " ")
+    );
+  }
+
   return result;
 };
