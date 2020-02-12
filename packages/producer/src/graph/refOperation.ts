9ea0680563ef77f789a565d8e931d887cdade5fc
Constantin Dumitrescu <constantin.dumitrescu@code11.com>
2020-02-12 13:48:03 +0200

    improvement(producer): remove setter apis from Ref
    These are redundant and additionally hide the intent of the producer

--- packages/producer/src/graph/refOperation.ts
+++ packages/producer/src/graph/refOperation.ts
@@ -18,32 +18,8 @@ export const refOperation = (
       return db.get(path);
     }
   };
-  const refMerge = (value: any, params: any) => {
-    const path = getInvokablePath(structure, op, params);
-    if (path) {
-      const patch = {
-        op: "merge",
-        path,
-        value: value,
-      };
-      db.patch([patch]);
-    }
-  };
-  const refSet = (value: any, params: any) => {
-    const path = getInvokablePath(structure, op, params);
-    if (path) {
-      const patch = {
-        op: "add",
-        path,
-        value: value,
-      };
-      db.patch([patch]);
-    }
-  };
   const ref = {
-    merge: refMerge,
     get: refGet,
-    set: refSet,
   };
 
   return ref;
