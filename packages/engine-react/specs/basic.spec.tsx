0685c0ec73069417047020cd96ad8e9a97604dd2
Constantin Dumitrescu <dum.constantin@gmail.com>
2020-03-10 15:08:21 +0200

    fix(engine-react): ensure producers are mounted and unmounted

---
+++ packages/engine-react/specs/basic.spec.tsx
@@ -0,0 +1,50 @@
+// tslint:disable:no-expression-statement
+import React from "react";
+import {
+  Get,
+  Set,
+  Ref,
+  Merge,
+  Prop,
+  Arg,
+  view,
+  producer,
+} from "@c11/engine.macro";
+import { waitForElement, getByTestId, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { Engine } from "../src/engine";
+
+jest.useFakeTimers();
+
+// @ts-ignore
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+});
+
+test("Simple load of a react component", done => {
+  const defaultState = {
+    foo: "123",
+  };
+  const rootEl = document.createElement("div");
+  rootEl.setAttribute("id", "root");
+  document.body.appendChild(rootEl);
+  const Component = view((foo = Get.foo) => {
+    return <div data-testid="foo">{foo}</div>;
+  });
+  const engine = new Engine({
+    state: {
+      initial: defaultState,
+    },
+    view: {
+      element: <Component />,
+      root: rootEl,
+    },
+  });
+  engine.start();
+  jest.runAllTimers();
+  waitForElement(() => getByTestId(document.body, "foo")).then(x => {
+    expect(x.innerHTML).toBe(defaultState.foo);
+    done();
+  });
+});
