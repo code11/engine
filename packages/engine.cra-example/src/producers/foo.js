baf4316b6c9ce536504ee4ad69c864573c5434f0
Constantin Dumitrescu <dum.constantin@gmail.com>
2020-10-31 02:21:13 +0200

    chore: remove example packages (#80)

--- packages/engine.cra-example/src/producers/foo.js
+++
@@ -1,6 +0,0 @@
-// import { producer } from "@c11/engine.macro";
-import { Get, Set } from "../state";
-
-export const foo = (val = Get.foo, setBar = Set.bar) => {
-  setBar(val + " copied from foo");
-};
--- packages/engine.cra-example/src/state.js
+++
@@ -1,7 +0,0 @@
-const Get = {};
-const Set = {};
-const Prop = {};
-const Ref = {};
-const Merge = {};
-
-export { Get, Set, Prop, Ref, Merge };
