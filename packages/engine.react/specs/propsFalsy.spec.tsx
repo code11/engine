89da6c4f810aa2e84fe31dc25b23e62183e18462
Constantin Dumitrescu <dum.constantin@gmail.com>
2020-11-09 00:44:59 +0200

    test: update dependencies to make sure tests work

--- packages/engine.react/specs/propsFalsy.spec.tsx
+++ packages/engine.react/specs/propsFalsy.spec.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { observe, update, view } from "@c11/engine.macro";
 import { waitFor, getByTestId, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { renderReact } from "../src";
