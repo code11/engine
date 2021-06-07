0ad62a8efc81450d36b66ba91c1640696e1fc5c5
Constantin Dumitrescu <dum.constantin@gmail.com>
2021-06-07 19:42:50 +0300

    fix(db): disallow paths with a wildcard

--- packages/engine.db/src/fn/isValidPath.ts
+++ packages/engine.db/src/fn/isValidPath.ts
@@ -1,7 +1,8 @@
 import isString from "lodash/isString";
 
 function isValidPath(path) {
-  return isString(path);
+  return isString(path) && !path.includes('*')
+  // console.log(path, isString(path) && /^(\/[a-z0-9~\\\-%^|"\ _]*)+$/gi.test(path))
   // return isString(path) && /^(\/[a-z0-9~\\\-%^|"\ _]*)+$/gi.test(path);
 }
 
