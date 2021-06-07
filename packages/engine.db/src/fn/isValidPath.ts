b99dc2ec9095ebf2ee13e94be09c1947fc6718c2
Constantin Dumitrescu <dum.constantin@gmail.com>
2021-06-07 12:13:04 +0300

    fix(db): allow non-json rfc paths

--- packages/engine.db/src/fn/isValidPath.ts
+++ packages/engine.db/src/fn/isValidPath.ts
@@ -1,7 +1,8 @@
 import isString from "lodash/isString";
 
 function isValidPath(path) {
-  return isString(path) && /^(\/[a-z0-9~\\\-%^|"\ _]*)+$/gi.test(path);
+  return isString(path);
+  // return isString(path) && /^(\/[a-z0-9~\\\-%^|"\ _]*)+$/gi.test(path);
 }
 
 export default isValidPath;
