bd2eae0af19982e812256da84730e2644375a20b
Constantin Dumitrescu <dum.constantin@gmail.com>
2022-07-12 18:20:42 +0300

    docs: add tracking code

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -52,9 +52,29 @@ function Feature({ imageUrl, title, description }) {
   );
 }
 
+const initTracking = () => {
+  var _paq = (window._paq = window._paq || []);
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+  _paq.push(["requireCookieConsent"]);
+  (function () {
+    var u = "https://c11engine.matomo.cloud/";
+    _paq.push(["setTrackerUrl", u + "matomo.php"]);
+    _paq.push(["setSiteId", "1"]);
+    var d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0];
+    g.async = true;
+    g.src = "//cdn.matomo.cloud/c11engine.matomo.cloud/matomo.js";
+    s.parentNode.insertBefore(g, s);
+  })();
+};
+
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
+  initTracking();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
