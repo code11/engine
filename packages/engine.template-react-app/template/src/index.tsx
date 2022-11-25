63d73b106049740449da49e4bb8fb6b2e1ad7a43
Constantin Dumitrescu <dum.constantin@gmail.com>
2022-11-25 13:05:42 +0200

    feat(engine.template-react-app): base setup

---
+++ packages/engine.template-react-app/template/src/index.tsx
@@ -0,0 +1,15 @@
+import { engine } from "@c11/engine.runtime";
+import { render } from "@c11/engine.react";
+import { App } from "./App";
+import "./global";
+
+const app = engine({
+  state: {},
+  use: [
+    render(<App />, "#app", {
+      debug: process.env.NODE_ENV === "development",
+    }),
+  ],
+});
+
+app.start();
